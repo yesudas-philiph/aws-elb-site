fe4600bf0a60e7a8cec1424119715c8c6122993b
Yousaf <31238282+yousafkhamza@users.noreply.github.com>
2021-06-08 10:44:51 +0530

    back to version 1

--- index.php
+++ index.php
@@ -36,7 +36,7 @@ https://www.tooplate.com/view/2114-pixie
                <?php
 $output = shell_exec('echo $HOSTNAME');
 echo "<h1><center><pre>$output</pre></center></h1>";
-echo "<h1><center>Version 2</center></h1>"
+echo "<h1><center>Version 1</center></h1>"
 ?>
             </span>
           </div>
